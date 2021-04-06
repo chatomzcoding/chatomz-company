03e04d17084a6eb9536172c9c5e7bbf1e078dd8a
chatomzcoding <coding.chatomz@gmail.com>
2021-04-06 17:48:09 +0700

    membuat helper pesan lewat whatsapp

--- app/Http/Helpers/Chatomz/market.php
+++ app/Http/Helpers/Chatomz/market.php
@@ -10,3 +10,15 @@ if (! function_exists('market_hitungdiskon')) {
         return $hargabaru;
     }
 }
+
+// link pesan lewat whatsapp
+if (! function_exists('market_pesanwhatsapp')) {
+    function market_pesanwhatsapp($no,$pesan)
+    {
+        // menggunakan fitur yang diberikan whatsapp secara gratis
+        $telp   = substr($no,1,11);
+        $pesan  = str_replace(' ','%20',$pesan);
+        $link   = "https://api.whatsapp.com/send?phone=62".$telp."&text=".$pesan;
+        return $link;
+    }
+}
