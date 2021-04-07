ff6b53cdf2d83ebbe9df3ecf66ddcc6f1f003b2a
chatomzcoding <coding.chatomz@gmail.com>
2021-04-07 10:05:21 +0700

    merubah icon menu sesuia dengan fungsinya

--- resources/views/sistem/menu.blade.php
+++ resources/views/sistem/menu.blade.php
@@ -1,6 +1,6 @@
 <li class="nav-item">
     <a href="#" class="nav-link">
-      <i class="nav-icon fas fa-heartbeat"></i>
+      <i class="nav-icon fas fa-user-secret"></i>
       <p>
         Chatomz
         <i class="fas fa-angle-left right"></i>
@@ -10,13 +10,13 @@
     <ul class="nav nav-treeview">
       <li class="nav-item">
         <a href="{{ url('/orang')}}" class="nav-link">
-          &nbsp;&nbsp;<i class="far fa-circle nav-icon"></i>
+          &nbsp;&nbsp;<i class="fas fa-user nav-icon"></i>
           <p>Orang</p>
         </a>
       </li>
       <li class="nav-item">
         <a href="{{ url('/keluarga')}}" class="nav-link">
-          &nbsp;&nbsp;<i class="far fa-circle nav-icon"></i>
+          &nbsp;&nbsp;<i class="fas fa-users nav-icon"></i>
           <p>Keluarga</p>
         </a>
       </li>
@@ -24,7 +24,7 @@
 </li>
 <li class="nav-item">
     <a href="#" class="nav-link">
-      <i class="nav-icon fas fa-cog"></i>
+      <i class="nav-icon fas fa-store-alt"></i>
       <p>
         Market
         <i class="fas fa-angle-left right"></i>
@@ -34,7 +34,7 @@
     <ul class="nav nav-treeview">
       <li class="nav-item">
         <a href="{{ url('/kategoriproduk')}}" class="nav-link">
-          &nbsp;&nbsp;<i class="far fa-circle nav-icon"></i>
+          &nbsp;&nbsp;<i class="fas fa-cubes nav-icon"></i>
           <p>Kategori Produk</p>
         </a>
       </li>
@@ -42,7 +42,7 @@
     <ul class="nav nav-treeview">
       <li class="nav-item">
         <a href="{{ url('/toko')}}" class="nav-link">
-          &nbsp;&nbsp;<i class="far fa-circle nav-icon"></i>
+          &nbsp;&nbsp;<i class="fas fa-store nav-icon"></i>
           <p>Toko</p>
         </a>
       </li>
@@ -50,7 +50,7 @@
     <ul class="nav nav-treeview">
       <li class="nav-item">
         <a href="{{ url('/produk')}}" class="nav-link">
-          &nbsp;&nbsp;<i class="far fa-circle nav-icon"></i>
+          &nbsp;&nbsp;<i class="fas fa-cube nav-icon"></i>
           <p>Produk</p>
         </a>
       </li>
@@ -58,7 +58,7 @@
 </li>
 <li class="nav-item">
     <a href="#" class="nav-link">
-      <i class="nav-icon fas fa-cog"></i>
+      <i class="nav-icon fas fa-user-tie"></i>
       <p>
         Admin
         <i class="fas fa-angle-left right"></i>
@@ -68,7 +68,7 @@
     <ul class="nav nav-treeview">
       <li class="nav-item">
         <a href="{{ url('/info-website')}}" class="nav-link">
-          &nbsp;&nbsp;<i class="far fa-circle nav-icon"></i>
+          &nbsp;&nbsp;<i class="fas fa-bullhorn nav-icon"></i>
           <p>Info Website</p>
         </a>
       </li>
@@ -76,7 +76,7 @@
     <ul class="nav nav-treeview">
       <li class="nav-item">
         <a href="{{ url('/adminuser')}}" class="nav-link">
-          &nbsp;&nbsp;<i class="far fa-circle nav-icon"></i>
+          &nbsp;&nbsp;<i class="fas fa-user nav-icon"></i>
           <p>User</p>
         </a>
       </li>
@@ -84,7 +84,7 @@
     <ul class="nav nav-treeview">
       <li class="nav-item">
         <a href="{{ url('/artikel')}}" class="nav-link">
-          &nbsp;&nbsp;<i class="far fa-circle nav-icon"></i>
+          &nbsp;&nbsp;<i class="fas fa-file-signature nav-icon"></i>
           <p>Artikel</p>
         </a>
       </li>
@@ -92,7 +92,7 @@
     <ul class="nav nav-treeview">
       <li class="nav-item">
         <a href="{{ url('/iklan')}}" class="nav-link">
-          &nbsp;&nbsp;<i class="far fa-circle nav-icon"></i>
+          &nbsp;&nbsp;<i class="fas fa-crow nav-icon"></i>
           <p>Iklan</p>
         </a>
       </li>
