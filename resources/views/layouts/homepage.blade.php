7d3c0555158906df784dd7cadc6d0922179a0925
chatomzcoding <coding.chatomz@gmail.com>
2021-04-14 10:53:54 +0700

    menambahkan section meta

--- resources/views/layouts/homepage.blade.php
+++ resources/views/layouts/homepage.blade.php
@@ -11,6 +11,7 @@
     <meta name="keywords" content="Ogani, unica, creative, html">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @yield('meta')
     <title>@yield('title')</title>
 
   <link rel="shortcut icon" href="{{ asset('/img/bunefit-v2.png')}}">
