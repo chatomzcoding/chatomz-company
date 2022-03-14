b214a7ddc4f3010a12b0fcc912113a068d78ec64
chatomzcoding <coding.chatomz@gmail.com>
2022-03-14 12:22:59 +0700

    percobaan component data

--- resources/views/chatomz/admin/dashboard.blade.php
+++ resources/views/chatomz/admin/dashboard.blade.php
@@ -8,6 +8,9 @@
       </div>
       <div class="page-content">
           <section class="row">
+              <div class="col-md-12">
+                <x-contoh/>
+              </div>
               <div class="col-12 col-lg-9">
                   <div class="row">
                       <div class="col-6 col-lg-3 col-md-6">
