9dd89f527cca282824d90e0a5d7288c31c86fdf2
chatomzcoding <coding.chatomz@gmail.com>
2022-03-14 10:55:39 +0700

    ubah ukuran Huruf

--- app/View/Components/sidebar.php
+++ app/View/Components/sidebar.php
@@ -4,7 +4,7 @@ namespace App\View\Components;
 
 use Illuminate\View\Component;
 
-class sidebar extends Component
+class Sidebar extends Component
 {
     /**
      * Create a new component instance.
